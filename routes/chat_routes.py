e0aeaa5eb67494d0b32e9bdbbd5fea8b09d0e8f3
RajanSingh-132 <singhrajan7052@gmail.com>
2026-03-09 13:30:12 +0530

    mod

--- routes/chat_routes.py
+++ routes/chat_routes.py
@@ -10,24 +10,9 @@ tracker.api_hit()
 
 @router.post("/chat")
 async def chat(data: dict):
-
-    print("Request received")
-
     user_message = data.get("message")
     user_id = "user1"
-
-    print("Message:", user_message)
-
     history = get_chat_history(user_id)
-
-    print("History loaded")
-
     ai_text = generate_ai_response(user_id, user_message, history)
-
-    print("AI generated")
-
     save_chat(user_id, user_message, ai_text)
-
-    print("Saved to DB")
-
     return {"response": ai_text}
